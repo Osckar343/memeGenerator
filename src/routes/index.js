474642761102f69e90ff32014794b61c00f42470
Oscar Mendez <oscarandres131@hotmail.com>
2021-08-18 21:24:10 -0500

    added uploader with drag and drop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
+
 var Scraper = require('images-scraper');
 
 const google = new Scraper({
@@ -8,7 +12,6 @@ const google = new Scraper({
     headless: false,
   },
 });
-
 
 router.get('/', (req, res) => {
     res.render('index.html', { title: 'First Website' }); 
@@ -23,6 +26,8 @@ router.post('/searching', async (req, res) => {
   }
 
     console.log(data);
+    console.log(data.file);
+    console.log(data.file.content);
 
     async function results () {
       const results = await google.scrape(data.topic, 1000);
@@ -32,14 +37,16 @@ router.post('/searching', async (req, res) => {
         const filteredResults = [];
 
         for (let i = 0; i < results.length; i++) {
-          if(results[i].url.includes('lookaside.fbsbx.com') || results[i].url.includes('i.ytimg.com')) {
-            console.log('YOUTUBE AND FACEBOOK FOUNDED');
+          if(results[i].url.includes(',')) {
+            filteredResults.push(results[i]);
           }
-          else {
+          /*if(results[i].url.includes('lookaside.fbsbx.com') || results[i].url.includes('i.ytimg.com')) {
+            console.log('YOUTUBE AND FACEBOOK FOUNDED');
+          } else {
             if(results[i].url.includes('dopl3r.com'))
               console.log('Dopler Encontrado en ' + results[i].url);
             filteredResults.push(results[i]);
-          }
+          }*/
             
         }
         return filteredResults;
@@ -50,14 +57,40 @@ router.post('/searching', async (req, res) => {
       }
     }
 
-    const urls = await results();
+    //const urls = await results();
 
     const info = {
       topic: data.topic,
-      results: urls
+      //results: urls
     }
 
   res.render('searching.html', { info: info }); 
+});
+
+router.get('/upload', (req, res) => {
+  res.render('upload.html', { title: 'First Website' }); 
+});
+
+router.post('/upload', upload.array('file', 50), function (req, res, next) {
+
+  var html = '';
+  for (let i = 0; i < req.files.length; i++) {
+    console.log(req.files[i].originalname);
+    let data = fs.readFileSync(req.files[i].path,'utf8');
+
+    const info = data.toString();
+    const arrayUrls = info.split(',');
+
+    for (let i = 0; i < arrayUrls.length; i++) {
+      arrayUrls[i] =  arrayUrls[i].replace(/¿/g,','); 
+    }
+
+    //Upload to database.
+    console.log(arrayUrls);
+    html += '<h2>' + req.files[i].originalname + '<h2>';
+  }
+
+  res.send(html);
 });
 
 module.exports = router;
